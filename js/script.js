8c87d81c785c878f9d16732a35c511f73c7c0928
Luca <lucafagiolini@live.it>
2024-01-25 16:19:18 +0100

    macro structure created

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// we need to generate a random password copmposed by:
+// 1. user first name
+// 2. user last name
+// 3. user favourite color
+
+// the result must be displayed in the page.
+
+
+
+
+
+
+
+
+// 1. name
+let firstName = prompt('inserisci qui il tuo nome');
+
+// 2. 
+
+
+
+
+
+
+
